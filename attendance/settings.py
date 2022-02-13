b9a83fab51c6f63c8e0d923c6ebf3056a3997cc6
Owen-Cochell <owencochell@gmail.com>
2022-02-13 00:07:24 -0500

    Added more Django production settings

--- attendance/settings.py
+++ attendance/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8(ve(p+55_-2&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['test.msuaiclub.com']
+ALLOWED_HOSTS = ['test.msuaiclub.com', 'localhost']
 
 
 # ASGI setting required for using `Channels` (for websockets)
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
