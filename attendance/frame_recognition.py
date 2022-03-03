a29eb3970805b3ccba2ef1c56d6c81506d2c0973
uzairname <uzair.m6d@gmail.com>
2022-03-02 20:18:47 -0500

    restructure files

---
+++ attendance/frame_recognition.py
@@ -0,0 +1,22 @@
+from recognition.py import HandTracker, SimpleFacerec
+
+#import the face recognition file
+
+import HandTracker
+
+class AnalyzeFrame():
+
+    """
+        Class that analyzes faces and hand gestures a frame
+
+
+    """
+    def __init__(self, frame):
+        self.frame = frame
+
+    def set_frame(self, frame):
+        pass
+
+
+
+
