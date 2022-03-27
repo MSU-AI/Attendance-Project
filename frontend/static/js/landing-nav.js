cbc4d83d72602ac2f64d8bb4b06324dd79daba52
ReeveFernandes <92554845+ReeveFernandes@users.noreply.github.com>
2022-03-27 17:41:49 -0400

    Delete frontend/static directory

--- frontend/static/js/landing-nav.js
+++
@@ -1,6 +0,0 @@
-let togglebtn = document.querySelector('.toggle-btn')
-        let navbar = document.querySelector('.navbar')
-
-        togglebtn.addEventListener('click', ()=>{
-            navbar.classList.toggle('mobile-nav')
-        } )
