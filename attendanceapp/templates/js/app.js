9e3f5d53f99ce3f904046248b1db1fbc86ec5875
ReeveFernandes <ferna312@msu.edu>
2022-03-27 22:52:38 -0400

    changed frontend folder to static

---
+++ attendanceapp/templates/js/app.js
@@ -0,0 +1,136 @@
+Webcam.set({
+    width: 640,
+    height: 600,
+    image_format: 'jpeg',
+    jpeg_quality: 100
+});
+// attach camera to the element
+Webcam.attach('#camera');
+
+/* Checking the protocal */
+let WS_URL
+if (location.protocol == 'http:'){
+    console.log("HTTP Connection")
+    WS_URL = "ws://localhost:8000/ws/camera";
+}
+
+if (location.protocol == 'https:'){
+    WS_URL =  "wss://test.msuaiclub.com:443/ws/camera"
+}
+
+// make connection
+const ws = new WebSocket(WS_URL);
+ws.onopen = () => {
+  console.log(`Connected to ${WS_URL}`);
+}
+
+// set camera click
+
+function click() {
+    
+    var i = 0
+    setInterval(function () {
+        
+        $(window).on('load', function(){
+            setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+          });
+        
+        takeSnapShot()
+        i = i+1
+        console.log(i);
+
+    },100) 
+}
+
+var flag = ''
+
+// TAKE A SNAPSHOT.
+takeSnapShot = function () {
+Webcam.snap(function (data_uri) {
+
+    
+    var str = data_uri
+    
+    // Here is where the metadata and payload are defined.
+    // Simply create the metadata as a JSON string with key 
+    // 'id' and value being the id of the event you wish to process.
+
+    // Send the image for face processing:
+
+    if (flag == 'face') {
+        ws.send('{"id": "face"}' + str);
+    }
+    
+
+    // Send the image for hand processing:
+    if (flag == 'hand') {
+        ws.send('{"id": "hand"}' + str);
+    }
+    
+
+    // Send data to the dummy handler:
+    if (flag == 'dummy') {
+        ws.send('{"id": "dummy"}This is dummy data!');
+    }
+    
+
+    // Raise an exception and show error handling
+    if (flag == 'error') {
+        ws.send('{"id": "error"}Raise an error!');
+    }
+    
+})};
+
+
+function handRec() {
+    flag = 'hand'
+    $('#info').text('Please give a Thumbs Up');
+    click()
+
+}
+
+function faceRec() {
+    flag = 'face'
+    $('#info').text("Please position your face");
+    click()
+}
+
+ws.onmessage = (message) => {
+
+    // Decode any metadata we sent:
+
+    meta = message.data.slice(0, message.data.indexOf('}')+1);
+
+    // Get payload data:
+
+    payload = message.data.slice(message.data.indexOf('}')+1);
+
+    // Output some stuff:
+
+    data = JSON.parse(payload);
+    /* console.log("Server says:", JSON.stringify(data));
+    console.log("Metadata: " + meta);
+    output.innerText = JSON.stringify(data); */
+
+    if (meta == 'hand') {
+        
+        if (JSON.stringify(data) == 'thumpsUp') {
+            
+            $('#info').text("Please position your face");
+            faceRec()
+
+        }
+    }
+
+    if (meta == 'face') {
+        
+        $('#info').text(JSON.stringify(data));
+        handRec()
+    }
+
+
+  };
+
+handRec()
+
+
