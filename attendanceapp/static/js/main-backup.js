4b91c29ccb53051308ca024be1402b69f03946c3
ReeveFernandes <ferna312@msu.edu>
2022-03-30 17:21:47 -0400

    added main-backup.js

---
+++ attendanceapp/static/js/main-backup.js
@@ -0,0 +1,198 @@
+/* Setting Webcam */
+Webcam.set({
+    width: 640,
+    height: 600,
+    image_format: 'jpeg',
+    jpeg_quality: 100
+});
+// attach camera to the element on screen
+Webcam.attach('#camera');
+
+ /* Checking the protocal for local and production testing*/
+let WS_URL
+if (location.protocol == 'http:'){
+
+    console.log("HTTP Connection")
+    WS_URL = "ws://localhost:8000/ws/camera";
+
+ }
+ 
+if (location.protocol == 'https:'){
+
+    WS_URL =  "wss://test.msuaiclub.com:443/ws/camera"
+
+ }
+ 
+/* setting up WebSocket Connection */
+const ws = new WebSocket(WS_URL);
+ws.onopen = () => {
+
+    // checking state of connection
+    console.log(`Connected to ${WS_URL}`);
+
+}
+ 
+
+/* on the click of this button: transition to Recog page */
+$(".ctn").click(function (e) { 
+    e.preventDefault();
+    $("#landing-container").css("display", "none");
+    $("#camera-page").css("display", "block");
+})
+
+
+/* Defining the event_loop */
+var flag = 'hand'
+function event_loop() {
+    
+    setInterval(() => {
+        
+        if (flag == 'hand') {
+            
+            $("#info").text("Starting Hand Rec...");
+            setTimeout(function handRec() {
+                $("#info").css("visibility", "visible");
+                $('#info').text('Please give a Thumbs Up');
+                setTimeout(click,3000);
+            }, 5000);
+        }
+
+
+        if (flag == 'face') {
+            
+            $("#info").text("Starting Face Rec...");
+            setTimeout(function faceRec() {
+                
+                $("#info").css("visibility", "visible");
+                $('#info').text("Please position your face");
+                setTimeout(click,3000)
+            }, 5000);
+        }
+    }, 2000);
+}
+
+
+/*  defining the click function */
+var i = 0 ;
+function click() {
+            
+    const click = setInterval(function () {
+        
+        setTimeout(takeSnapShot, 2000);
+        i = i+1;
+        console.log(i);
+
+    },750);
+}
+
+
+/* defining takeSnapShot */
+takeSnapShot = function () {
+    Webcam.snap(function (data_uri) {
+    
+        
+        var str = data_uri
+        
+        // Here is where the metadata and payload are defined.
+        // Simply create the metadata as a JSON string with key 
+        // 'id' and value being the id of the event you wish to process.
+    
+        // Send the image for face processing:
+    
+        if (flag == 'face') {
+            ws.send('{"id": "face", "token": ' + i + '}' + str);
+        }
+        
+    
+        // Send the image for hand processing:
+        if (flag == 'hand') {
+            ws.send('{"id": "hand", "token": ' + i + '}' + str);
+        }
+        
+    
+        // Send data to the dummy handler:
+        if (flag == 'dummy') {
+            ws.send('{"id": "dummy"}This is dummy data!');
+        }
+        
+    
+        // Raise an exception and show error handling
+        if (flag == 'error') {
+            ws.send('{"id": "error"}Raise an error!');
+        }
+        
+    })};
+
+
+ws.onmessage = (message) => {
+    
+    // Decode any metadata we sent:
+
+    meta = message.data.slice(0, message.data.indexOf('}')+1);
+
+    // Get payload data:
+
+    payload = message.data.slice(message.data.indexOf('}')+1);
+
+    // Output some stuff:
+
+    data = JSON.parse(payload);
+    meta_data = JSON.parse(meta);
+    console.log("Server says:", JSON.stringify(data));
+    console.log("Metadata: " + meta);
+
+    console.log(meta_data['id']);
+        
+    if (meta_data['id'] == 'hand' && flag == 'hand') {
+        
+        if (data[0] == 'thumbs up') {
+
+            clearInterval(click)
+            console.log("Face recogntion now!");
+            
+            $('#info').text("Please position your face");
+            flag = 'face';
+
+        }
+    }
+
+    if (meta_data['id'] == 'face') {
+                
+        clearInterval(click)        
+        var name = JSON.stringify(data).slice(52,-3)
+        if (name == 'unknown') {
+
+            
+            eventFlag = false
+            $("#link").css("display", "inline-block");
+            $("#continue").css("display", "inline-block");
+            $("#name-unknown-box").css("display", "block");
+            $("#info").css("display", "none");
+
+            nameDisplay = "Sorry, you were not recognised."
+            $(".name-display").text(nameDisplay);
+
+            $("#continue").click(function (e) { 
+                e.preventDefault();
+                $("#name-unknown-box").css("display", "none");
+                $("#info").css("display", "block");
+            });
+        } else{
+
+            $("#link").css("display", "none");
+            $("#name-unknown-box").css("display", "block");
+            nameDisplay = "Name: " + name
+            $('#name-display').text(nameDisplay);
+            setTimeout(() => {
+
+                $("#name-unknown-box").css("display", "none");
+                $("#info").css("display", "block");
+
+            },8000)
+        }
+
+}   
+flag = 'hand'
+}    
+
+event_loop();
