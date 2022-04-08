534a558310beb711460c7b3e89a4645c3f611f8b
ReeveFernandes <ferna312@msu.edu>
2022-04-07 22:45:14 -0400

    navbar sucks

--- attendanceapp/static/js/landing-nav.js
+++ attendanceapp/static/js/landing-nav.js
@@ -1,6 +1,11 @@
+if (window.innerWidth < 600) {
+    
+    $(".navbar").css("display", "none");
+
+}
 let togglebtn = document.querySelector('.toggle-btn')
         let navbar = document.querySelector('.navbar')
 
         togglebtn.addEventListener('click', ()=>{
-            navbar.classList.toggle("hidden")
+            navbar.classList.toggle("show")
         } )
