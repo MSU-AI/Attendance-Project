4ba343d713461f4db57e6ae33f1ce474e0bbd7b3
ReeveFernandes <ferna312@msu.edu>
2022-04-07 23:02:38 -0400

    navbar resolved

--- attendanceapp/static/js/landing-nav.js
+++ attendanceapp/static/js/landing-nav.js
@@ -1,16 +1,8 @@
-if (window.innerWidth < 600) {
-    
-    $(".navbar").css("display", "none");
-    $(".nav-list").css("display", "none");
-
-
-}
 let togglebtn = document.querySelector('.toggle-btn')
-        let navbar = document.querySelector('.navbar')
-        let navlist = document.querySelector('.nav-list')
+let navbar = document.querySelector('.navbar')
 
         togglebtn.addEventListener('click', ()=>{
-            navbar.classList.toggle("show")
-            navlist.classList.toggle("show")
+            navbar.classList.toggle("mobile-nav")
+
 
         } )
